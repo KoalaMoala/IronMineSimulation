8bcd9b020e58da64c5f0fa5f11c8881edf7cbd5c
mytev <tevainui.miyou@gmail.com>
2016-04-11 19:50:05 -0400

    Simplification du code

--- BehaviourTree.py
+++ BehaviourTree.py
@@ -1,3 +1,22 @@
-class BehaviourTree:
-    def __init__(self):
-        self.seq = []
+from bt.base import *
+from condition import *
+from action import *
+from bt.decorator import *
+
+def createBehaviorTree(noeud):
+    x = machineTache(noeud)
+    condition1 = entreeTest(noeud)
+
+    deco = Delay(noeud.tcycle)
+    deco.add_child(x)
+
+    racine = SequenceStar()
+    root= Repeater()
+    root.add_child(racine)
+
+    selector1 = Sequence()
+    selector1.add_child(condition1)
+    racine.add_child(selector1)
+    racine.add_child(deco)
+
+
