d6c14957b5c430b90a526f90fd8a6f09d3cf74d0
KoalaMoala <nicolas.amelot@outlook.com>
2016-04-21 13:52:43 -0400

    clean BT, unused function

--- BehaviourTree.py
+++ BehaviourTree.py
@@ -16,30 +16,6 @@ class BehaviorTree():
             self.createBTMixer(noeud)
         elif(noeud.name == "Loading Dock"):
             self.createBTLoadingDock(noeud)
-        else:
-            self.createBehaviorTree(noeud)
-
-    def createBehaviorTree(self, noeud):
-        x = machineTache(noeud)
-        deco = Delay(noeud.tcycle)
-        deco.add_child(x)
-
-        self.root = SequenceStar()
-
-        selector1 = Sequence()
-        for i in range(len(noeud.ina)):
-            selector1.add_child(entreeTest(noeud,i))
-
-        repeater = Repeater()
-        selector2 = Sequence()
-        for i in range(len(noeud.oua)):
-            selector2.add_child(sortieTest(noeud,i))
-        selector2.add_child(machinePostTache(noeud))
-        repeater.add_child(selector2)
-
-        self.root.add_child(selector1)
-        self.root.add_child(deco)
-        self.root.add_child(repeater)
 
     def createBTUnloadingDock(self, noeud):
         self.root = SequenceStar()
