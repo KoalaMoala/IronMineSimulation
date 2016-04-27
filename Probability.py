21ba8028f104e011e19fbe2db39c396b79f6c790
mytev <tevainui.miyou@gmail.com>
2016-04-27 11:54:03 -0400

    [CLEAN] Unused code probability.py

--- Probability.py
+++ Probability.py
@@ -7,14 +7,6 @@ class DistribTriang:
         self._mode = amode
         self._max = amax
 
-    def fct(self,x):
-        if(x >= self._min and x <= self._mode):
-            return 2 * (x-self._min) / ((self._max - self._min)*(self._mode - self._min))
-        elif(x>self._mode and x<self._max):
-            return 2 * (self._max-x) / ((self._max - self._min)*(self._max - self._mode))
-        else:
-            return 0
-
     def mcInv(self):
         _U = random.random()
         _fc = (self._mode - self._min) / (self._max - self._min)
